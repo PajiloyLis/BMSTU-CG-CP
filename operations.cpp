1d09e49b5f15fe77a5022ddd9a07e7fd24c99308
Bugakov Ivan <bugakov0601@yandex.ru>
2024-11-30 08:45:01 +0300

    auto commit

--- operations.cpp
+++ operations.cpp
@@ -5,11 +5,14 @@ void z_buffer(array<my_vec3f, 3> points, vector<vector<sf::Vertex>> &image, sf::
     if (points[0].getZ() > points[1].getZ()) std::swap(points[0], points[1]);
     if (points[0].getZ() > points[2].getZ()) std::swap(points[0], points[2]);
     if (points[1].getZ() > points[2].getZ()) std::swap(points[1], points[2]);
-    int total_height = (int) round(points[2].getZ() - points[0].getZ());
+    points[0].setZ(ceil(points[0].getZ())), points[0].setX(ceil(points[0].getX())), points[0].setY(ceil(points[0].getY()));
+    points[1].setZ(ceil(points[1].getZ())), points[1].setX(ceil(points[1].getX())), points[1].setY(ceil(points[1].getY()));
+    points[2].setZ(ceil(points[2].getZ())), points[2].setX(ceil(points[2].getX())), points[2].setY(ceil(points[2].getY()));
+    int total_height = (int) (points[2].getZ() - points[0].getZ());
     for (int i = 0; i < total_height; i++) {
         bool second_half = i > points[1].getZ() - points[0].getZ() || points[1].getZ() == points[0].getZ();
-        int segment_height = (second_half ? static_cast<int>(round(points[2].getZ() - points[1].getZ()))
-                                          : static_cast<int>(round(points[1].getZ() - points[0].getZ())));
+        int segment_height = (second_half ? static_cast<int>(points[2].getZ() - points[1].getZ())
+                                          : static_cast<int>(points[1].getZ() - points[0].getZ()));
         float alpha = (float) i / total_height;
         float beta = (float) (i - (second_half ? points[1].getZ() - points[0].getZ() : 0)) /
                      segment_height; // be careful: with above conditions no division by zero here
@@ -17,14 +20,14 @@ void z_buffer(array<my_vec3f, 3> points, vector<vector<sf::Vertex>> &image, sf::
         my_vec3f B = second_half ? points[1] + my_vec3f(points[2] - points[1]) * beta :
                      points[0] + my_vec3f(points[1] - points[0]) * beta;
         if (A.getY() > B.getY()) std::swap(A, B);
-        for (int j = static_cast<int>(round(A.getY())); j <= static_cast<int>(round(B.getY())); j++) {
+        for (int j = static_cast<int>(A.getY()); j <= static_cast<int>(B.getY()); j++) {
             float phi = B.getY() == A.getY() ? 1. : (float) (j - A.getY()) / (float) (B.getY() - A.getY());
             my_vec3f P = my_vec3f(A) + my_vec3f(B - A) * phi;
             if (P.getZ() >= 0 && P.getY() >= 0 && P.getZ() < image.size() && P.getY() < image[0].size()) {
                 int idx = static_cast<int>(round(P.getY() + P.getZ() * image[0].size()));
                 if (z_buffer[idx] < P.getX()) {
                     z_buffer[idx] = P.getX();
-                    image[static_cast<int>(round(P.getZ()))][static_cast<int>(round(P.getY()))].color = color;
+                    image[static_cast<int>((P.getZ()))][static_cast<int>(round(P.getY()))].color = color;
                 }
             }
         }
