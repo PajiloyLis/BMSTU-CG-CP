4f11139e9e63501c5a32af435e339a2283f0b9cc
Bugakov Ivan <bugakov0601@yandex.ru>
2024-11-30 05:55:01 +0300

    auto commit

--- operations.h
+++ operations.h
@@ -1,4 +1,14 @@
 #ifndef CP_CG_OPERATIONS_H
 #define CP_CG_OPERATIONS_H
 
+#include <array>
+#include <iostream>
+#include <algorithm>
+#include "point.h"
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+
 #endif //CP_CG_OPERATIONS_H
