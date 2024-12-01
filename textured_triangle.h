e0cca8bc30f14540c92ea5160933a1335b1877ff
Bugakov Ivan <bugakov0601@yandex.ru>
2024-12-01 19:10:01 +0300

    auto commit

--- textured_triangle.h
+++ textured_triangle.h
@@ -1,7 +1,3 @@
-//
-// Created by ivan on 01.12.24.
-//
-
 #ifndef CP_CG_TEXTURED_TRIANGLE_H
 #define CP_CG_TEXTURED_TRIANGLE_H
 
@@ -10,5 +6,3 @@ class textured_triangle {
 
 };
 
-
-#endif //CP_CG_TEXTURED_TRIANGLE_H
