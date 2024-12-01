779af4e0ea63730febf7998b3a3140a36b1d5c33
Bugakov Ivan <bugakov0601@yandex.ru>
2024-12-01 19:15:01 +0300

    auto commit

--- textured_triangle.cpp
+++ textured_triangle.cpp
@@ -1,5 +1,1 @@
-//
-// Created by ivan on 01.12.24.
-//
-
 #include "textured_triangle.h"
--- textured_triangle.h
+++ textured_triangle.h
@@ -1,8 +1,15 @@
 #ifndef CP_CG_TEXTURED_TRIANGLE_H
 #define CP_CG_TEXTURED_TRIANGLE_H
 
+#include"triangle.h"
+#include<SFML/Graphics.hpp>
+#include<SFML/Graphics/Image.hpp>
 
 class textured_triangle {
-
+    textured_triangle(const my_vec3f &normal, const array<)
+private:
+    triangle t;
+    sf::Sprite sprite;
 };
 
+#endif
