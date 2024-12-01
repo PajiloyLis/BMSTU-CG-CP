4309f3dc32d28337c1fabda36d8db64fd4ed9574
Bugakov Ivan <bugakov0601@yandex.ru>
2024-12-01 19:09:52 +0300

    gitignore

---
+++ textured_triangle.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ivan on 01.12.24.
+//
+
+#include "textured_triangle.h"
---
+++ textured_triangle.h
@@ -0,0 +1,14 @@
+//
+// Created by ivan on 01.12.24.
+//
+
+#ifndef CP_CG_TEXTURED_TRIANGLE_H
+#define CP_CG_TEXTURED_TRIANGLE_H
+
+
+class textured_triangle {
+
+};
+
+
+#endif //CP_CG_TEXTURED_TRIANGLE_H
