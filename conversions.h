b340701bf3c9657d7d2a2e9de5d9f93339b69577
Bugakov Ivan <bugakov0601@yandex.ru>
2024-12-02 19:40:01 +0300

    auto commit

--- conversions.h
+++ conversions.h
@@ -1,14 +1,23 @@
 #ifndef CP_CG_CONVERSIONS_H
 #define CP_CG_CONVERSIONS_H
 
+#include <glm/glm.hpp>
+
+using namespace glm
+
 struct move_t
 {
     double dx, dy, dz;
+
 };
 
-struct rotate_t
+class rotate_t
 {
-    double angle_x, angle_y, angle_z;
+    rotate_t(const double &x, const double&y, const double &z)
+    {
+        mat3x3 x_rotate()
+    }
+    ;
 };
 
 struct scale_t
