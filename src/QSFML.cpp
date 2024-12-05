601aa111333173917512bcdaf184016d6945cd50
Bugakov Ivan <bugakov0601@yandex.ru>
2024-12-05 17:45:01 +0300

    auto commit

--- src/QSFML.cpp
+++ src/QSFML.cpp
@@ -132,8 +132,8 @@ void QSFMLCanvas::Clear() {
 }
 
 my_vec3f QSFMLCanvas::viewport(const my_vec3f &point) {
-    mat4 mat(this->size().width() / 2.f, 0, 0, point.getX(),
-             0, this->size().height() / 2.f, 0, point.getY(),
+    mat4 mat(this->size().width() / 2.f, 0, 0, this->size().width()/2.f,
+             0, this->size().height() / 2.f, 0, this->size().height()/2.f,
              0, 0, 1, 0,
              0, 0, 0, 1);
     vec4 res = mat * vec4(point.getX(), point.getY(), point.getZ(), 1);
