194f8840680f07ad9483afa212468bbc5592e04f
Bugakov Ivan <bugakov0601@yandex.ru>
2024-12-05 22:15:01 +0300

    auto commit

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -11,6 +11,7 @@ MainWindow::MainWindow() : QMainWindow(), handler() {
     drawer->setObjectName("drawer");
     drawer->setGeometry(menu->pos().x(), grid->pos().y()+menu->size().height(),
                         grid->size().width(), grid->size().height());
+    drawer->setMouseTracking(true);
     this->handler.AddCamera({120, 0, 0, static_cast<float>(drawer->size().width())/drawer->size().height()});
     this->handler.SetScene(
             Scene(drawer, drawer->size().width(), drawer->size().height()));
