5728e100b0d7a7df7fc6b89aa511f64775444319
Bugakov Ivan <bugakov0601@yandex.ru>
2024-12-06 03:45:01 +0300

    auto commit

--- src/QSFML.cpp
+++ src/QSFML.cpp
@@ -138,6 +138,8 @@ void QSFMLCanvas::Clear() {
     this->clear(back_color);
 }
 
+my_vec3f QSFMLCanvas::ad
+
 my_vec3f QSFMLCanvas::viewport(const my_vec3f &point, const my_vec3f &center) {
     mat4 mat(this->size().width() / (2 * center.getX()), 0, 0, point.getX() + this->size().width() / 2.f,
              0, this->size().height() / (2 * center.getY()), 0, point.getY() + this->size().height() / 2.f,
