8215a60ccde22e8f96f6e9934f629b4c07e9bf49
Bugakov Ivan <bugakov0601@yandex.ru>
2025-02-16 18:55:01 +0300

    auto commit

--- inc/camera.h
+++ inc/camera.h
@@ -4,37 +4,120 @@
 #include <my_vec3f.h>
 #include "conversions.h"
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class camera {
 public:
-    camera() = default;
+    // Камера позиция, цель и вектор "вверх"
+    glm::vec3 position;
+    glm::vec3 front;
+    glm::vec3 up;
+    glm::vec3 right;
 
-camera(const my_vec3f &pos, const my_vec3f &pov, const my_vec3f &global_up, float relation);
+    // Углы эйлера для вращения камеры
+    float yaw;
+    float pitch;
 
-    camera(const my_vec3f &pos, const my_vec3f &pov, const my_vec3f &global_up, float width, float height);
+    // Скорость движения
+    float movementSpeed;
+    float mouseSensitivity;
 
-    camera(const camera &c);
+    camera(glm::vec3 startPosition, glm::vec3 startUp,
+           float startYaw, float startPitch)
+            : front(glm::vec3(0.0f, 0.0f, -1.0f)),
+              movementSpeed(2.5f), mouseSensitivity(0.1f) {
+        position = startPosition;
+        up = startUp;
+        yaw = startYaw;
+        pitch = startPitch;
+        updateCameraVectors();
+    }
 
-    camera& operator=(const camera &);
+    // Обновление векторов камеры на основе углов эйлера
+    void updateCameraVectors() {
+        // Calculate the new Front vector
+        glm::vec3 front;
+        front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+        front.y = sin(glm::radians(pitch));
+        front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+        this->front = glm::normalize(front);
 
-    camera(camera &&c) noexcept;
+        // Also re-calculate the Right and Up vector
+        right = glm::normalize(glm::cross(this->front, up));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+        up = glm::normalize(glm::cross(right, this->front));
+    }
 
-    [[nodiscard]] mat4 perspective() const;
+    // Обработка движения камеры
+    void processKeyboard(char direction, float deltaTime) {
+        float velocity = movementSpeed * deltaTime;
+        if (direction == 'W') // Move forward
+            position += front * velocity;
+        if (direction == 'S') // Move backward
+            position -= front * velocity;
+        if (direction == 'A') // Move left
+            position -= right * velocity;
+        if (direction == 'D') // Move right
+            position += right * velocity;
+    }
 
-    [[nodiscard]] mat4 camLookAt() const;
+    // Обработка движения мыши
+    void processMouseMovement(float xOffset, float yOffset, GLboolean constrainPitch = true) {
+        xOffset *= mouseSensitivity;
+        yOffset *= mouseSensitivity;
 
-    void rotate(const rotate_t &rotate);
+        yaw += xOffset;
+        pitch -= yOffset;
 
-    void scale(float &scale);
+        // Убедимся, что "pitch" остается в пределах
+        if (constrainPitch) {
+            if (pitch > 89.0f)
+                pitch = 89.0f;
+            if (pitch < -89.0f)
+                pitch = -89.0f;
+        }
 
-    my_vec3f pos, front, up, right, global_up;
-    float near_distance = 0.1;
-    float relation;
-    float angle_of_view=45;
-    float far_distance = 100;
+        updateCameraVectors();
+    }
 
-    void move(const move_t &move);
+    // Получение матрицы вида
+    glm::mat4 getViewMatrix() {
+        return glm::lookAt(position, position + front, up);
+    }
 };
 
-
+//
+//class camera {
+//public:
+//    camera() = default;
+//
+//camera(const my_vec3f &pos, const my_vec3f &pov, const my_vec3f &global_up, float relation);
+//
+//    camera(const my_vec3f &pos, const my_vec3f &pov, const my_vec3f &global_up, float width, float height);
+//
+//    camera(const camera &c);
+//
+//    camera& operator=(const camera &);
+//
+//    camera(camera &&c) noexcept;
+//
+//    [[nodiscard]] mat4 perspective() const;
+//
+//    [[nodiscard]] mat4 camLookAt() const;
+//
+//    void rotate(const rotate_t &rotate);
+//
+//    void scale(float &scale);
+//
+//    my_vec3f pos, front, up, right, global_up;
+//    float near_distance = 0.1;
+//    float relation;
+//    float angle_of_view=45;
+//    float far_distance = 100;
+//
+//    void move(const move_t &move);
+//};
+//
+//
 #endif //CP_CG_CAMERA_H
--- src/camera.cpp
+++ src/camera.cpp
@@ -1,85 +1,85 @@
 #include "camera.h"
 
-camera::camera(const my_vec3f &pos, const my_vec3f &pov, const my_vec3f &global_up, float relation) {
-    this->pos = pos;
-    this->relation = relation;
-    this->front = (pos - pov).normalize();
-    this->right = global_up.cross(front).normalize();
-    this->up = (front.cross(right)).normalize();
-    this->global_up = global_up;
-    cout << "pos " << this->pos << "\npov " << this->front << "\nright " << right << "\nup " << up << '\n';
-}
-
-
-camera::camera(const camera &c) {
-    pos = c.pos;
-    front = c.front;
-    right = c.right;
-    up = c.up;
-    relation = c.relation;
-    global_up = c.global_up;
-    near_distance = c.near_distance;
-}
-
-camera::camera(camera &&c) noexcept {
-    pos = c.pos;
-    front = c.front;
-    right = c.right;
-    up = c.up;
-    relation = c.relation;
-    global_up = c.global_up;
-    near_distance = c.near_distance;
-}
-
-mat4 camera::perspective() const {
-    return glm::perspective(radians(angle_of_view), relation, near_distance, far_distance);
-}
-
-camera::camera(const my_vec3f &pos, const my_vec3f &pov, const my_vec3f &global_up, float width, float height) {
-    this->pos = pos;
-    this->relation = width / height;
-    this->front = (pos - pov).normalize();
-    this->right = global_up.cross(front).normalize();
-    this->up = (front.cross(right)).normalize();
-    this->global_up = global_up;
-    cout << "pos " << this->pos << "\npov " << this->front << "\nright " << right << "\nup " << up << '\n';
-}
-
-camera &camera::operator=(const camera &c) {
-    pos = c.pos;
-    front = c.front;
-    right = c.right;
-    up = c.up;
-    relation = c.relation;
-    global_up = c.global_up;
-    near_distance = c.near_distance;
-    return *this;
-}
-
-mat4 camera::camLookAt() const {
-    return glm::lookAt(pos.p, (pos - front).p, up.p);
-}
-
-void camera::rotate(const rotate_t &rotate) {
-    front.rotate(rotate).normalize();
-    right = (global_up.cross(front)).normalize();
-    up = (front.cross(right)).normalize();
-    cout << "camera rotated\npos " << pos << "\npov " << front << '\n';
-}
-
-void camera::move(const move_t &move) {
-//    my_vec3f tmp = pos;
-    pos += front * move.dz;
-    pos += right * move.dx;
-    pos += up * move.dy;
-    cout << "camera moved \npos " << pos << "\npov " << front << '\n';
-}
-
-void camera::scale(float &scale) {
-    angle_of_view += scale;
-    if (angle_of_view < 1.0f)
-        angle_of_view = 1.0f;
-    if (angle_of_view > 45.0f)
-        angle_of_view = 45.0f;
-    cout << "camera scaled " << angle_of_view << '\n';
-}
+//camera::camera(const my_vec3f &pos, const my_vec3f &pov, const my_vec3f &global_up, float relation) {
+//    this->pos = pos;
+//    this->relation = relation;
+//    this->front = (pos - pov).normalize();
+//    this->right = global_up.cross(front).normalize();
+//    this->up = (front.cross(right)).normalize();
+//    this->global_up = global_up;
+//    cout << "pos " << this->pos << "\npov " << this->front << "\nright " << right << "\nup " << up << '\n';
+//}
+//
+//
+//camera::camera(const camera &c) {
+//    pos = c.pos;
+//    front = c.front;
+//    right = c.right;
+//    up = c.up;
+//    relation = c.relation;
+//    global_up = c.global_up;
+//    near_distance = c.near_distance;
+//}
+//
+//camera::camera(camera &&c) noexcept {
+//    pos = c.pos;
+//    front = c.front;
+//    right = c.right;
+//    up = c.up;
+//    relation = c.relation;
+//    global_up = c.global_up;
+//    near_distance = c.near_distance;
+//}
+//
+//mat4 camera::perspective() const {
+//    return glm::perspective(radians(angle_of_view), relation, near_distance, far_distance);
+//}
+//
+//camera::camera(const my_vec3f &pos, const my_vec3f &pov, const my_vec3f &global_up, float width, float height) {
+//    this->pos = pos;
+//    this->relation = width / height;
+//    this->front = (pos - pov).normalize();
+//    this->right = global_up.cross(front).normalize();
+//    this->up = (front.cross(right)).normalize();
+//    this->global_up = global_up;
+//    cout << "pos " << this->pos << "\npov " << this->front << "\nright " << right << "\nup " << up << '\n';
+//}
+//
+//camera &camera::operator=(const camera &c) {
+//    pos = c.pos;
+//    front = c.front;
+//    right = c.right;
+//    up = c.up;
+//    relation = c.relation;
+//    global_up = c.global_up;
+//    near_distance = c.near_distance;
+//    return *this;
+//}
+//
+//mat4 camera::camLookAt() const {
+//    return glm::lookAt(pos.p, (pos - front).p, up.p);
+//}
+//
+//void camera::rotate(const rotate_t &rotate) {
+//    front.rotate(rotate).normalize();
+//    right = (global_up.cross(front)).normalize();
+//    up = (front.cross(right)).normalize();
+//    cout << "camera rotated\npos " << pos << "\npov " << front << '\n';
+//}
+//
+//void camera::move(const move_t &move) {
+////    my_vec3f tmp = pos;
+//    pos += front * move.dz;
+//    pos += right * move.dx;
+//    pos += up * move.dy;
+//    cout << "camera moved \npos " << pos << "\npov " << front << '\n';
+//}
+//
+//void camera::scale(float &scale) {
+//    angle_of_view += scale;
+//    if (angle_of_view < 1.0f)
+//        angle_of_view = 1.0f;
+//    if (angle_of_view > 45.0f)
+//        angle_of_view = 45.0f;
+//    cout << "camera scaled " << angle_of_view << '\n';
+//}
