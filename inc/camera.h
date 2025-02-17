6251becd7c67d9204a0b9c00ea0daa344aff56b3
Bugakov Ivan <bugakov0601@yandex.ru>
2025-02-17 09:40:01 +0300

    auto commit

--- inc/camera.h
+++ inc/camera.h
@@ -73,7 +73,7 @@ public:
 
     // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
     void move(Camera_Movement direction, float delta_time) {
-        float velocity = MovementSpeed * delta_time;
+        float velocity = MovementSpeed;
         if (direction == FORWARD)
             Position += Front * velocity;
         if (direction == BACKWARD)
