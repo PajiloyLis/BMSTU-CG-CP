dd560107f4d8d4a4445fec373cd93041b61a65b4
Bugakov Ivan <bugakov0601@yandex.ru>
2025-02-17 03:20:01 +0300

    auto commit

--- inc/camera.h
+++ inc/camera.h
@@ -18,10 +18,10 @@ enum Camera_Movement {
 };
 
 // Default camera values
-const float YAW = -90.0f;
+const float YAW = 0.0f;
 const float PITCH = 0.0f;
 const float SPEED = 2.5f;
-const float SENSITIVITY = 0.05f;
+const float SENSITIVITY = 0.01f;
 const float ZOOM = 45.0f;
 
 
