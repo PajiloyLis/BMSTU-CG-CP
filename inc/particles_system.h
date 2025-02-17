b9bc6aa6df3bb6f44b5e41deeee7803bf5a7cfdb
Bugakov Ivan <bugakov0601@yandex.ru>
2025-02-17 11:25:01 +0300

    auto commit

---
+++ inc/particles_system.h
@@ -0,0 +1,15 @@
+//
+// Created by ivan on 17.02.25.
+//
+
+#ifndef CP_CG_PARTICLES_SYSTEM_H
+#define CP_CG_PARTICLES_SYSTEM_H
+
+
+class particles_system {
+public:
+    
+};
+
+
+#endif //CP_CG_PARTICLES_SYSTEM_H
---
+++ src/particles_system.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ivan on 17.02.25.
+//
+
+#include "particles_system.h"
