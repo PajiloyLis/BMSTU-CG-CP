c9f8b77aecb71d966b2feda8db2e09affd0880bb
Bugakov Ivan <bugakov0601@yandex.ru>
2025-02-19 11:40:01 +0300

    auto commit

--- src/particles_system.cpp
+++ src/particles_system.cpp
@@ -8,10 +8,10 @@ particles_system::particles_system(const int &start_count, const glm::vec3 &spaw
                                    const glm::vec3 &speed) {
     gen = mt19937(rd());
     particles.resize(start_count, particle(spawn, 1.f,
-                                           speed + glm::vec3(2 - (gen() % 5), 2 - (gen() % 5), 2 - (gen() % 5)),
-//                                           {static_cast<sf::Uint8 >(color.r + 1 - gen() % 3),
-//                                            static_cast<sf::Uint8 >(color.g + 1 - gen() % 3),
-//                                            static_cast<sf::Uint8 >(color.b + 1 - gen() % 3)}
+                                           speed + glm::vec3(2 - (rand() % 5), 2 - (rand() % 5), 2 - (rand() % 5)),
+//                                           {static_cast<sf::Uint8 >(color.r + 1 - rand() % 3),
+//                                            static_cast<sf::Uint8 >(color.g + 1 - rand() % 3),
+//                                            static_cast<sf::Uint8 >(color.b + 1 - rand() % 3)}
                                            sf::Color::Black));
     this->spawn = spawn;
     timer_id = 0;
@@ -33,14 +33,14 @@ particles_system &particles_system::operator=(const particles_system &system) {
 void particles_system::update_coords(const int &add_count) {
     for (int i = 0; i < add_count; ++i) {
         particles.push_back(particle(spawn + glm::vec3(2 - (rand() % 5), 2 - (rand() % 5), 2 - (rand() % 5)), 1.f,
-                                     particles[0].speed + glm::vec3(2 - gen() % 5, 2 - gen() % 5, 2 - gen() % 5),
-//                                           {static_cast<sf::Uint8 >(color.r + 1 - gen() % 3),
-//                                            static_cast<sf::Uint8 >(color.g + 1 - gen() % 3),
-//                                            static_cast<sf::Uint8 >(color.b + 1 - gen() % 3)}
+                                     particles[0].speed + glm::vec3(2 - rand() % 5, 2 - rand() % 5, 2 - rand() % 5),
+//                                           {static_cast<sf::Uint8 >(color.r + 1 - rand() % 3),
+//                                            static_cast<sf::Uint8 >(color.g + 1 - rand() % 3),
+//                                            static_cast<sf::Uint8 >(color.b + 1 - rand() % 3)}
                                      sf::Color::Black));
     }
     for (auto &particle: particles) {
-        particle.position += particle.speed + glm::vec3(2 - gen() % 5, 2 - gen() % 5, 2 - gen() % 5);
+        particle.position += particle.speed + glm::vec3(2 - rand() % 5, 2 - rand() % 5, 2 - rand() % 5);
 //        cout << particle.position.x << ' '<<particle.position.y << " "<<particle.position.z << '\n';
     }
 }
