f88b96377dae2ae8f8d931a8d3e19b2e785f1809
Bugakov Ivan <bugakov0601@yandex.ru>
2025-02-19 10:20:01 +0300

    auto commit

--- src/particles_system.cpp
+++ src/particles_system.cpp
@@ -9,9 +9,10 @@ particles_system::particles_system(const int &start_count, const glm::vec3 &spaw
     gen = mt19937(rd());
     particles.resize(start_count, particle(spawn + glm::vec3(2 - gen() % 5, 2 - gen() % 5, 2 - gen() % 5), 1.f,
                                            speed + glm::vec3(2 - gen() % 5, 2 - gen() % 5, 2 - gen() % 5),
-                                           {static_cast<sf::Uint8 >(color.r + 1 - gen() % 3),
-                                            static_cast<sf::Uint8 >(color.g + 1 - gen() % 3),
-                                            static_cast<sf::Uint8 >(color.b + 1 - gen() % 3)}));
+//                                           {static_cast<sf::Uint8 >(color.r + 1 - gen() % 3),
+//                                            static_cast<sf::Uint8 >(color.g + 1 - gen() % 3),
+//                                            static_cast<sf::Uint8 >(color.b + 1 - gen() % 3)}
+                                            sf::Color::Black));
     this->spawn = spawn;
     timer_id = 0;
 }
