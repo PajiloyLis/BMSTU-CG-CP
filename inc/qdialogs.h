aa3714807d76e94ff93c574909ecafc970a3f59f
Bugakov Ivan <bugakov0601@yandex.ru>
2025-02-25 02:35:01 +0300

    auto commit

--- inc/qdialogs.h
+++ inc/qdialogs.h
@@ -5,9 +5,47 @@
 #ifndef CP_CG_QDIALOGS_H
 #define CP_CG_QDIALOGS_H
 
+#include <QApplication>
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QSlider>
 
-class qdialogs {
-    
+class WindDialog : public QDialog {
+    Q_OBJECT
+
+public:
+    WindDialog(QWidget *parent = nullptr) : QDialog(parent) {
+        setWindowTitle("Установить параметры ветра");
+
+        QLabel *label = new QLabel("E");
+        QSlider *slider = new QSlider(Qt::Horizontal);
+        slider->setRange(0, 35);
+        slider->setValue(5); // Начальное значение
+        QLineEdit *inputField = new QLineEdit(this);
+        QPushButton *okButton = new QPushButton("OK", this);
+        QPushButton *cancelButton = new QPushButton("Cancel", this);
+
+        QVBoxLayout *mainLayout = new QVBoxLayout(this);
+        mainLayout->addWidget(label);
+        mainLayout->addWidget(inputField);
+
+        QHBoxLayout *buttonLayout = new QHBoxLayout();
+        buttonLayout->addWidget(okButton);
+        buttonLayout->addWidget(cancelButton);
+
+        mainLayout->addLayout(buttonLayout);
+
+        connect(okButton, &QPushButton::clicked, this, &WindDialog::accept);
+        connect(cancelButton, &QPushButton::clicked, this, &WindDialog::reject);
+    }
+
+    QString getInputText() const {
+        return findChild<QLineEdit *>())->text();
+    }
 };
 
 
