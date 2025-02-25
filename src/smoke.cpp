142b3ae511fbba937fb49da776b5e313b5f0c4ac
Bugakov Ivan <bugakov0601@yandex.ru>
2025-02-25 19:00:01 +0300

    auto commit

--- src/smoke.cpp
+++ src/smoke.cpp
@@ -168,8 +168,8 @@ void smoke::update() {
     for (int i = 0; i < height + 2; ++i) {
         for (int j = 0; j < height + 2; ++j) {
             for (int k = 0; k < width + 2; ++k) {
-                u_prev[i][j][k] = log(i) * wind.y;
-                v_prev[i][j][k] = log(i) * wind.x;
+                u_prev[i][j][k] = (i> 0 ? log(i) * wind.y:0);
+                v_prev[i][j][k] = (i>0 ? log(i) * wind.x : 0);
                 w_prev[i][j][k] = 0.001;
                 dens_prev[i][j][k] = 0;
             }
