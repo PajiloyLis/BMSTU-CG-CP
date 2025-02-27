0d16ba0315a72275a2f9854909f4f4af4fb984c8
Bugakov Ivan <bugakov0601@yandex.ru>
2025-02-27 15:20:01 +0300

    auto commit

--- inc/cp_cg_ui.h
+++ inc/cp_cg_ui.h
@@ -13,9 +13,10 @@
 #include <QtGui/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenu>
-#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSlider>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -31,17 +32,29 @@ public:
     QWidget *centralwidget;
     QWidget *gridLayoutWidget;
     QGridLayout *gridLayout;
-    QMenuBar *menubar;
-    QMenu *model_menu;
-    QMenu *params_menu;
-    QMenu *sim_menu;
+    QLabel *label_11;
+    QLabel *label_5;
+    QSlider *wind_angle_slider;
+    QLabel *label_10;
+    QLabel *wind_speed_label;
+    QLabel *label_7;
+    QSlider *sim_speed_slider;
+    QLabel *label_3;
+    QLabel *label_9;
+    QSlider *wind_speed_slider;
+    QLabel *label_6;
+    QLabel *label_2;
+    QLabel *sim_speed_label;
+    QPushButton *load_model_button;
+    QLabel *label;
+    QLabel *wind_angle_label;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(1850, 1016);
+        MainWindow->resize(635, 626);
         load_model_action = new QAction(MainWindow);
         load_model_action->setObjectName(QString::fromUtf8("load_model_action"));
         sim_start = new QAction(MainWindow);
@@ -54,32 +67,143 @@ public:
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         gridLayoutWidget = new QWidget(centralwidget);
         gridLayoutWidget->setObjectName(QString::fromUtf8("gridLayoutWidget"));
-        gridLayoutWidget->setGeometry(QRect(0, 0, 1841, 971));
+        gridLayoutWidget->setGeometry(QRect(10, 10, 611, 591));
         gridLayout = new QGridLayout(gridLayoutWidget);
         gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
         gridLayout->setContentsMargins(0, 0, 0, 0);
+        label_11 = new QLabel(gridLayoutWidget);
+        label_11->setObjectName(QString::fromUtf8("label_11"));
+        label_11->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(label_11, 8, 2, 1, 1);
+
+        label_5 = new QLabel(gridLayoutWidget);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(label_5->sizePolicy().hasHeightForWidth());
+        label_5->setSizePolicy(sizePolicy);
+        label_5->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(label_5, 4, 0, 1, 3);
+
+        wind_angle_slider = new QSlider(gridLayoutWidget);
+        wind_angle_slider->setObjectName(QString::fromUtf8("wind_angle_slider"));
+        wind_angle_slider->setMinimum(0);
+        wind_angle_slider->setMaximum(359);
+        wind_angle_slider->setSliderPosition(0);
+        wind_angle_slider->setOrientation(Qt::Horizontal);
+        wind_angle_slider->setTickPosition(QSlider::TicksBelow);
+        wind_angle_slider->setTickInterval(10);
+
+        gridLayout->addWidget(wind_angle_slider, 5, 1, 1, 1);
+
+        label_10 = new QLabel(gridLayoutWidget);
+        label_10->setObjectName(QString::fromUtf8("label_10"));
+        label_10->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(label_10, 8, 0, 1, 1);
+
+        wind_speed_label = new QLabel(gridLayoutWidget);
+        wind_speed_label->setObjectName(QString::fromUtf8("wind_speed_label"));
+        wind_speed_label->setEnabled(true);
+        QSizePolicy sizePolicy1(QSizePolicy::Preferred, QSizePolicy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(wind_speed_label->sizePolicy().hasHeightForWidth());
+        wind_speed_label->setSizePolicy(sizePolicy1);
+        wind_speed_label->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(wind_speed_label, 3, 0, 1, 3);
+
+        label_7 = new QLabel(gridLayoutWidget);
+        label_7->setObjectName(QString::fromUtf8("label_7"));
+        label_7->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(label_7, 5, 2, 1, 1);
+
+        sim_speed_slider = new QSlider(gridLayoutWidget);
+        sim_speed_slider->setObjectName(QString::fromUtf8("sim_speed_slider"));
+        sim_speed_slider->setMinimum(1);
+        sim_speed_slider->setMaximum(5);
+        sim_speed_slider->setSliderPosition(1);
+        sim_speed_slider->setOrientation(Qt::Horizontal);
+        sim_speed_slider->setTickPosition(QSlider::TicksBelow);
+        sim_speed_slider->setTickInterval(1);
+
+        gridLayout->addWidget(sim_speed_slider, 8, 1, 1, 1);
+
+        label_3 = new QLabel(gridLayoutWidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(label_3, 2, 2, 1, 1);
+
+        label_9 = new QLabel(gridLayoutWidget);
+        label_9->setObjectName(QString::fromUtf8("label_9"));
+        sizePolicy.setHeightForWidth(label_9->sizePolicy().hasHeightForWidth());
+        label_9->setSizePolicy(sizePolicy);
+        label_9->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(label_9, 7, 0, 1, 3);
+
+        wind_speed_slider = new QSlider(gridLayoutWidget);
+        wind_speed_slider->setObjectName(QString::fromUtf8("wind_speed_slider"));
+        wind_speed_slider->setMinimum(0);
+        wind_speed_slider->setMaximum(25);
+        wind_speed_slider->setSliderPosition(0);
+        wind_speed_slider->setOrientation(Qt::Horizontal);
+        wind_speed_slider->setTickPosition(QSlider::TicksBelow);
+        wind_speed_slider->setTickInterval(1);
+
+        gridLayout->addWidget(wind_speed_slider, 2, 1, 1, 1);
+
+        label_6 = new QLabel(gridLayoutWidget);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+        label_6->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(label_6, 5, 0, 1, 1);
+
+        label_2 = new QLabel(gridLayoutWidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(label_2, 2, 0, 1, 1);
+
+        sim_speed_label = new QLabel(gridLayoutWidget);
+        sim_speed_label->setObjectName(QString::fromUtf8("sim_speed_label"));
+        sizePolicy1.setHeightForWidth(sim_speed_label->sizePolicy().hasHeightForWidth());
+        sim_speed_label->setSizePolicy(sizePolicy1);
+        sim_speed_label->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(sim_speed_label, 9, 0, 1, 3);
+
+        load_model_button = new QPushButton(gridLayoutWidget);
+        load_model_button->setObjectName(QString::fromUtf8("load_model_button"));
+
+        gridLayout->addWidget(load_model_button, 0, 0, 1, 3);
+
+        label = new QLabel(gridLayoutWidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        sizePolicy.setHeightForWidth(label->sizePolicy().hasHeightForWidth());
+        label->setSizePolicy(sizePolicy);
+        label->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(label, 1, 0, 1, 3);
+
+        wind_angle_label = new QLabel(gridLayoutWidget);
+        wind_angle_label->setObjectName(QString::fromUtf8("wind_angle_label"));
+        sizePolicy1.setHeightForWidth(wind_angle_label->sizePolicy().hasHeightForWidth());
+        wind_angle_label->setSizePolicy(sizePolicy1);
+        wind_angle_label->setAlignment(Qt::AlignCenter);
+
+        gridLayout->addWidget(wind_angle_label, 6, 0, 1, 3);
+
         MainWindow->setCentralWidget(centralwidget);
-        menubar = new QMenuBar(MainWindow);
-        menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 1850, 22));
-        model_menu = new QMenu(menubar);
-        model_menu->setObjectName(QString::fromUtf8("model_menu"));
-        params_menu = new QMenu(menubar);
-        params_menu->setObjectName(QString::fromUtf8("params_menu"));
-        sim_menu = new QMenu(menubar);
-        sim_menu->setObjectName(QString::fromUtf8("sim_menu"));
-        MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
         MainWindow->setStatusBar(statusbar);
-
-        menubar->addAction(model_menu->menuAction());
-        menubar->addAction(params_menu->menuAction());
-        menubar->addAction(sim_menu->menuAction());
-        model_menu->addAction(load_model_action);
-        params_menu->addAction(set_wind_action);
-        params_menu->addAction(set_speed_action);
-        sim_menu->addAction(sim_start);
 
         retranslateUi(MainWindow);
 
@@ -93,9 +217,19 @@ public:
         sim_start->setText(QCoreApplication::translate("MainWindow", "\320\235\320\260\321\207\320\260\321\202\321\214", nullptr));
         set_wind_action->setText(QCoreApplication::translate("MainWindow", "\320\227\320\260\320\264\320\260\321\202\321\214 \320\262\320\265\321\202\320\265\321\200", nullptr));
         set_speed_action->setText(QCoreApplication::translate("MainWindow", "\320\227\320\260\320\264\320\260\321\202\321\214 \321\201\320\272\320\276\321\200\320\276\321\201\321\202\321\214 \321\201\320\270\320\274\321\203\320\273\321\217\321\206\320\270\320\270", nullptr));
-        model_menu->setTitle(QCoreApplication::translate("MainWindow", "\320\234\320\276\320\264\320\265\320\273\321\214", nullptr));
-        params_menu->setTitle(QCoreApplication::translate("MainWindow", "\320\237\320\260\321\200\320\260\320\274\320\265\321\202\321\200\321\213", nullptr));
-        sim_menu->setTitle(QCoreApplication::translate("MainWindow", "\320\241\320\270\320\274\321\203\320\273\321\217\321\206\320\270\321\217", nullptr));
+        label_11->setText(QCoreApplication::translate("MainWindow", "X5", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "\320\227\320\260\320\264\320\260\321\202\321\214 \320\275\320\260\320\277\321\200\320\260\320\262\320\273\320\265\320\275\320\270\320\265 \320\262\320\265\321\202\321\200\320\260 (\320\260\320\267\320\270\320\274\321\203\321\202)", nullptr));
+        label_10->setText(QCoreApplication::translate("MainWindow", "X1", nullptr));
+        wind_speed_label->setText(QCoreApplication::translate("MainWindow", "0 \320\274/\321\201", nullptr));
+        label_7->setText(QCoreApplication::translate("MainWindow", "360", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "35", nullptr));
+        label_9->setText(QCoreApplication::translate("MainWindow", "\320\222\321\213\320\261\321\200\320\260\321\202\321\214 \321\201\320\272\320\276\321\200\320\276\321\201\321\202\321\214 \321\201\320\270\320\274\321\203\320\273\321\217\321\206\320\270\320\270", nullptr));
+        label_6->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "0", nullptr));
+        sim_speed_label->setText(QCoreApplication::translate("MainWindow", "X1", nullptr));
+        load_model_button->setText(QCoreApplication::translate("MainWindow", "\320\227\320\260\320\263\321\200\321\203\320\267\320\270\321\202\321\214 \320\274\320\276\320\264\320\265\320\273\321\214", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "\320\227\320\260\320\264\320\260\321\202\321\214 \321\201\320\272\320\276\321\200\320\276\321\201\321\202\321\214 \320\262\320\265\321\202\321\200\320\260 (\320\274/\321\201)", nullptr));
+        wind_angle_label->setText(QCoreApplication::translate("MainWindow", "0 \302\260", nullptr));
     } // retranslateUi
 
 };
