8889babfbbd2df17a320b87bc05510706f52a4ad
Bugakov Ivan <bugakov0601@yandex.ru>
2025-02-27 22:50:01 +0300

    auto commit

--- src/operations.cpp
+++ src/operations.cpp
@@ -1,16 +1,16 @@
 #include "operations.h"
 
-void z_buffer(array<glm::vec3, 3> points, Image &image, sf::Color color, vector<float> &z_buffer) {
-    if (points[0].z == points[1].z && points[1].z == points[2].z) return;
-    if (points[0].z > points[1].z) std::swap(points[0], points[1]);
+void z_buffer(array<glm::vec3, 3> points, sf::RenderTarget &image, sf::Color color, vector<float> &z_buffer) {
+    glm::vec<3, int, glm::defaultp> t0(ceil(points[0].x), ceil(points[0].y), ceil(points[0].z)),
+            t1(ceil(points[1].x), ceil(points[1].y), ceil(points[1].z)),
+            t2(ceil(points[2].x), ceil(points[2].y), ceil(points[2].z));
+    if (t0.y == t1.y && t1.z == t2.z) return;
+    if (points[0].н > points[1].z) std::swap(points[0], points[1]);
     if (points[0].z > points[2].z) std::swap(points[0], points[2]);
     if (points[1].z > points[2].z) std::swap(points[1], points[2]);
-    points[0].z = (ceil(points[0].z)), points[0].x = (ceil(points[0].x)), points[0].y = (
-            ceil(points[0].y));
-    points[1].z = (ceil(points[1].z)), points[1].x = (ceil(points[1].x)), points[1].y = (
-            ceil(points[1].y));
-    points[2].z = (ceil(points[2].z)), points[2].x = (ceil(points[2].x)), points[2].y = (
-            ceil(points[2].y));
+    glm::vec<3, int, glm::defaultp> t0(ceil(points[0].x), ceil(points[0].y), ceil(points[0].z)),
+            t1(ceil(points[1].x), ceil(points[1].y), ceil(points[1].z)),
+            t2(ceil(points[2].x), ceil(points[2].y), ceil(points[2].z));
     int total_height = (int) (points[2].z - points[0].z);
     for (int i = 0; i < total_height; i++) {
         bool second_half = i > points[1].z - points[0].z || points[1].z == points[0].z;
@@ -21,7 +21,7 @@ void z_buffer(array<glm::vec3, 3> points, Image &image, sf::Color color, vector<
                      segment_height; // be careful: with above conditions no division by zero here
         glm::vec3 A = points[0] + glm::vec3(points[2] - points[0]) * alpha;
         glm::vec3 B = second_half ? points[1] + glm::vec3(points[2] - points[1]) * beta :
-                     points[0] + glm::vec3(points[1] - points[0]) * beta;
+                      points[0] + glm::vec3(points[1] - points[0]) * beta;
         if (A.y > B.y) std::swap(A, B);
         for (int j = static_cast<int>(A.y); j <= static_cast<int>(B.y); j++) {
             float phi = B.y == A.y ? 1. : (float) (j - A.y) / (float) (B.y - A.y);
