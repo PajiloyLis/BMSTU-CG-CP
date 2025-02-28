af0028fbead3ef37ba79c05c663b276ab1eea426
Bugakov Ivan <bugakov0601@yandex.ru>
2025-02-28 17:20:01 +0300

    auto commit

--- src/operations.cpp
+++ src/operations.cpp
@@ -1,10 +1,10 @@
 #include "operations.h"
 
 void z_buffer(array<glm::vec3, 3> points, sf::RenderTarget &image, sf::Color color, vector<float> &z_buffer) {
-    glm::vec<3, int, glm::defaultp> t0(round(points[0].x), round(points[0].y), round(points[0].z)),
-            t1(round(points[1].x), round(points[1].y), round(points[1].z)),
-            t2(round(points[2].x), round(points[2].y), round(points[2].z));
-    if (t0.y == t1.y && t1.z == t2.z) return;
+    if (abs(points[0].y - points[1].y) < 1e-5 && abs(points[1].y - points[2].y) < 1e-5) return;
+    glm::vec<3, int, glm::defaultp> t0(ceil(points[0].x), ceil(points[0].y), ceil(points[0].z)),
+            t1(ceil(points[1].x), ceil(points[1].y), ceil(points[1].z)),
+            t2(ceil(points[2].x), ceil(points[2].y), ceil(points[2].z));
     if (t0.y > t1.y) std::swap(t0, t1);
     if (t0.y > t2.y) std::swap(t0, t2);
     if (t1.y > t2.y) std::swap(t1, t2);
@@ -15,15 +15,15 @@ void z_buffer(array<glm::vec3, 3> points, sf::RenderTarget &image, sf::Color col
         float alpha = (float) i / total_height;
         float beta = (float) (i - (second_half ? t1.y - t0.y : 0)) /
                      segment_height; // be careful: with above conditions no division by zero here
-        glm::vec<3, int, glm::defaultp> A = glm::vec3(t0) + glm::vec3(t2 - t0) * alpha;
-        glm::vec<3, int, glm::defaultp> B = (second_half ? glm::vec3(t1) + glm::vec3(t2 - t1) * beta :
-                                             glm::vec3(t0) + glm::vec3(t1 - t0) * beta);
+        glm::vec3 A = glm::vec3(t0) + glm::vec3(t2 - t0) * alpha;
+        glm::vec3 B = (second_half ? glm::vec3(t1) + glm::vec3(t2 - t1) * beta :
+                       glm::vec3(t0) + glm::vec3(t1 - t0) * beta);
         if (A.x > B.x) std::swap(A, B);
-        for (int j = A.x; j <= B.x; j++) {
+        for (int j = round(A.x); j <= round(B.x); j++) {
             float phi = B.x == A.x ? 1. : (float) (j - A.x) / (float) (B.x - A.x);
-            glm::vec<3, int, glm::defaultp> P = glm::vec3(A) + glm::vec3(B - A) * phi;
-            int idx = P.x + P.y * image.getSize().x;
-            if (idx>=0 && idx <z_buffer.size() && z_buffer[idx] < P.z) {
+            glm::vec3 P = glm::vec3(A) + glm::vec3(B - A) * phi;
+            int idx = round(P.x + P.y * image.getSize().x);
+            if (idx >= 0 && idx < z_buffer.size() && z_buffer[idx] < P.z) {
                 z_buffer[idx] = P.z;
                 image.draw(vector<sf::Vertex>(1, sf::Vertex(sf::Vector2f(P.x, image.getSize().y - P.y), color)).data(),
                            1,
