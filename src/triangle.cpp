0c15b002e132c56e20fe4df66bad255f1275e0c3
Bugakov Ivan <bugakov0601@yandex.ru>
2025-03-01 04:10:01 +0300

    auto commit

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -91,6 +91,10 @@ triangle::draw(sf::RenderTarget &target, vector<float> &zbuffer, const glm::vec3
     for (int i = 0; i < 3; ++i) {
         to_draw[i] = sf::Vertex(sf::Vector2f(vertices[i].x, vertices[i].y), colors[i]);
     }
-    target.draw(to_draw);
+    glBegin(GL_TRIANGLES);
+    glColor3ui(colors[0].r, colors[0].g, colors[0].b); glVertex3f(vertices[0].x, vertices[0].y, vertices[0].z);
+    glColor3ui(0, 1, 0); glVertex3f(vertices[1].x, vertices[1].y, vertices[1].z);
+    glColor3ui(0, 0, 1); glVertex3f(vertices[2].x, vertices[2].y, vertices[2].z);
+    glEnd();
 #endif
 }
