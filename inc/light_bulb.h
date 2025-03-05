6ed5c135e631da5cde916be1ba1882eb935bfbb6
Bugakov Ivan <bugakov0601@yandex.ru>
2025-03-05 17:20:01 +0300

    auto commit

---
+++ inc/light_bulb.h
@@ -0,0 +1,14 @@
+//
+// Created by ivan on 05.03.25.
+//
+
+#ifndef CP_CG_LIGHT_BULB_H
+#define CP_CG_LIGHT_BULB_H
+
+
+class light_bulb {
+
+};
+
+
+#endif //CP_CG_LIGHT_BULB_H
---
+++ src/light_bulb.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ivan on 05.03.25.
+//
+
+#include "light_bulb.h"
